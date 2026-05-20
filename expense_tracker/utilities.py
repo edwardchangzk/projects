9167ec2151ad2452241b93e37fb3db57a00c4501
ed <edward.changzk@gmail.com>
2026-05-20 20:09:41 +0800

    Create utilities.py

---
+++ expense_tracker/utilities.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+
+def validate_date(date_text):
+
+    try:
+        datetime.strptime(
+            date_text,
+            "%Y-%m-%d"
+        )
+
+        return True
+
+    except ValueError:
+        return False
